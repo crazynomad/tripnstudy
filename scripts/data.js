b0d6390458622a45ca929dded25d14f4e47fd66c
zhenbinsang <zhenbinsang@energysource-cn.com>
2015-04-04 19:52:40 +0800

    update for 20150404

---
+++ scripts/data.js
@@ -0,0 +1,27 @@
+var _infodata=[
+						{"curr":true,"bgc":"004787","name":"哥伦比亚大学30天学习","imgurl":["img/ts1.jpg"],
+							"features":"30天，超长常青藤实地学习、生活体验；住哥伦比亚大学学生宿舍；所有哥伦比亚设施免费使用；实战百所大学招生会","age":1,"agedesc":"16-18岁","place":5,"placedesc":"纽约、哥伦比亚大学、耶鲁大学、普林斯顿大学、宾夕法尼亚大学",
+							"placeimgs":["img/transparent.png"],"topic":2,"topicdesc":"常青藤校专项学习（经济、金融、科技创新、数学与工程、演讲与辩论、美国大学申请与SAT）、名校生活体验、观光旅行","agency":"环识国际",
+							"host":4,"date":"2015年7月12日","price":"69800(RMB)","link":"#","read":8},
+						{"curr":false,"bgc":"00a0c2","name":"西海岸科技创新","imgurl":["img/ts2.jpg"],
+							"features":"斯坦福、NASA实验室、太阳观测站、科学院、Discovery等权威活动","age":2,"agedesc":"14-18岁岁","place":4,"placedesc":"旧金山、斯坦福、San Diego、迪士尼、环球影城",
+							"placeimgs":["img/transparent.png"],"topic":4,"topicdesc":"科技创新、英语学习、观光旅行","agency":"环识国际",
+							"host":2,"date":"暂缺","price":"暂缺","link":"#","read":88},
+						{"curr":false,"bgc":"00a74d","name":"东海岸亲子团","imgurl":["img/ts3.jpg"],
+							"features":"私立寄宿制高中考察、投资移民讲座、哈佛耶鲁MIKT名校参观、顶级SPA享受","age":3,"agedesc":"12-16岁","place":3,"placedesc":"华盛顿、白宫、纽约、波士顿、哈佛大学、耶鲁大学、麻省理工学院",
+							"placeimgs":["img/transparent.png"],"topic":1,"topicdesc":"学校考察、投资移民、观光旅行","agency":"环识国际",
+							"host":5,"date":"暂缺","price":"暂缺","link":"#","read":888},
+						{"curr":false,"bgc":"a8171a","name":"佛罗里达","imgurl":["img/ts4.jpg"],
+							"features":"国内罕见的户外拓展课程、全球最大的迪士尼乐园Y.E.S青少年领导力课程","age":4,"agedesc":"暂缺","place":2,"placedesc":"暂缺",
+							"placeimgs":["img/transparent.png"],"topic":3,"topicdesc":"户外拓展、领导力、环境保护、观光旅行","agency":"环识国际",
+							"host":3,"date":"暂缺","price":"暂缺","link":"#","read":8888},
+						{"curr":false,"bgc":"6a59a7","name":"洛杉矶","imgurl":["img/ts5.jpg"],
+							"features":"暂缺","age":5,"agedesc":"10-14岁","place":1,"placedesc":"洛杉矶、旧金山、迪士尼、好莱坞",
+							"placeimgs":["img/transparent.png"],"topic":5,"topicdesc":"英语学习、观光旅行","agency":"英孚教育",
+							"host":1,"date":"2015年7月16日-8月5日日","price":"43397(RMB)","link":"#","read":88888},
+						{"curr":false,"bgc":"6a59a7","name":"纽约","imgurl":["img/ts5.jpg"],
+							"features":"暂缺","age":5,"agedesc":"9-13岁，14-18岁","place":1,"placedesc":"纽约、哈佛大学、麻省理工学院",
+							"placeimgs":["img/transparent.png"],"topic":5,"topicdesc":"英语学习、观光旅行","agency":"英孚教育",
+							"host":1,"date":"2015年7月中下旬","price":"暂缺","link":"#","read":88888}
+					]
+					
